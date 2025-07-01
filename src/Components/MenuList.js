b18bd7dd88a90202bac0c4ca1f2d2d83906bb347
Tofik Mifta <130132150+tofik-93@users.noreply.github.com>
2025-07-01 13:14:05 +0300

    Update MenuList.js

--- src/Components/MenuList.js
+++ src/Components/MenuList.js
@@ -1,41 +1,57 @@
-import {BsFillHouseFill, BsFillHousesFill, BsJournalAlbum} from "react-icons/bs";
-import {FaBroadcastTower, FaMicrophoneAlt, FaPodcast} from "react-icons/fa";
-import {BiPlus} from "react-icons/bi";
+import { BiPlus } from "react-icons/bi";
+import { BsFillHouseFill, BsJournalAlbum, BsMusicNoteList } from "react-icons/bs";
+import { FaBroadcastTower, FaMicrophoneAlt, FaPodcast } from "react-icons/fa";
 
+interface MenuItem {
+  id: number;
+  icon: JSX.Element;
+  name: string;
+  description: string;
+}
 
-const MenuList = [
+const MenuList: MenuItem[] = [
   {
-    id:1,
-    icon:<BsFillHouseFill/>,
-    name:"Home"
-  },
- 
-  {
-    id:2,
-    icon:<BiPlus/>,
-    name:"Discover"
+    id: 1,
+    icon: <BsFillHouseFill />,
+    name: "Home",
+    description: "Go to the homepage",
   },
   {
-    id:3,
-    icon:<FaBroadcastTower/>,
-    name:"Radio"
+    id: 2,
+    icon: <BiPlus />,
+    name: "Discover",
+    description: "Explore new music and content",
   },
   {
-    id:4,
-    icon:<FaMicrophoneAlt/>,
-    name:"Artist"
+    id: 3,
+    icon: <FaBroadcastTower />,
+    name: "Radio",
+    description: "Listen to live radio stations",
   },
   {
-    id:5,
-    icon:<BsJournalAlbum/>,
-    name:"Album"
+    id: 4,
+    icon: <FaMicrophoneAlt />,
+    name: "Artist",
+    description: "Browse artists and their music",
   },
   {
-    id:6,
-    icon:<FaPodcast/>,
-    name:"podcast"
+    id: 5,
+    icon: <BsJournalAlbum />,
+    name: "Album",
+    description: "View albums and tracklists",
   },
-  
-]
+  {
+    id: 6,
+    icon: <FaPodcast />,
+    name: "Podcast",
+    description: "Access podcasts and episodes",
+  },
+  {
+    id: 7,
+    icon: <BsMusicNoteList />,
+    name: "Playlists",
+    description: "Manage and explore playlists",
+  },
+];
 
-export {MenuList}
+export { MenuList };
